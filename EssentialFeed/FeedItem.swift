06e06a2f9d69f8c7cc4680953e3e9ad48fdcdbd8
Alex Shekunsky <oleksandr.shchekunskyi@windmill.ch>
2023-05-31 14:59:26 +0300

    Added FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  EssentialFeed
+//
+//  Created by Alex2 on 31.05.2023.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let url: URL
+}
