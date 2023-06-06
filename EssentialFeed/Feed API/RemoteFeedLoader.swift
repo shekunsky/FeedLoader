5e6420f5ac2e6c64c17a0459c74245515de55c70
Alex Shekunsky <oleksandr.shchekunskyi@windmill.ch>
2023-06-06 16:02:44 +0300

    Track for memory leaks

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -30,14 +30,18 @@ public final class RemoteFeedLoader {
         client.get(from: url) { result in
             switch result {
             case let .success(data, response):
-                if let items = try? FeedItemsMapper.map(data, response) {
-                    completion(.success(items))
-                } else {
-                    completion(.failure(.invalidData))
-                }
+                completion(self.map(data, from: response))
             case .failure:
                 completion(.failure(.connectivityError))
             }
+        }
+    }
+    
+    private func map(_ data: Data, from response: HTTPURLResponse) -> Result {
+        if let items = try? FeedItemsMapper.map(data, response) {
+            return .success(items)
+        } else {
+            return .failure(.invalidData)
         }
     }
 }
