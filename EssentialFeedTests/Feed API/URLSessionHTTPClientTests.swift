bc84a56f86bb1cd1bb19aaf5d336993af0d35d06
Alex Shekunsky <oleksandr.shchekunskyi@windmill.ch>
2023-06-09 17:41:07 +0300

    Move the URLSessionHTTPClient (the system under test, or "SUT") creaton to a factory method to protect our tests from breaking changes

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -38,7 +38,6 @@ class URLSessionHTTPClientTests: XCTestCase {
     }
     
     func test_getFromURL_performsGetRequestWithURL() {
-        
         let url = URL(string: "http://some-url.com")!
         let exp = expectation(description:"Wait for completion")
         
@@ -48,7 +47,8 @@ class URLSessionHTTPClientTests: XCTestCase {
             
             exp.fulfill()
         }
-        URLSessionHTTPClient().get(from: url) { _ in }
+        
+        makeSUT().get(from: url) { _ in }
         
         wait(for: [exp], timeout: 1.0)
     }
@@ -56,14 +56,11 @@ class URLSessionHTTPClientTests: XCTestCase {
     func test_getFromURL_failsOnRequestError() {
         let url = URL(string: "http://some-url.com")!
         let error = NSError(domain: "any error", code: 1)
+        let exp = expectation(description:"Wait for completion")
         
         URLProtocolStub.stub(data: nil, response: nil, error: error)
         
-        let sut = URLSessionHTTPClient()
-        
-        let exp = expectation(description:"Wait for completion")
-        
-        sut.get(from: url) { result in
+        makeSUT().get(from: url) { result in
             switch result {
             case let .failure(receivedError as NSError):
                 XCTAssertEqual(receivedError.domain, error.domain)
@@ -78,6 +75,10 @@ class URLSessionHTTPClientTests: XCTestCase {
     }
     
     // MARK: - Helpers
+    
+    private func makeSUT() -> URLSessionHTTPClient {
+        return URLSessionHTTPClient()
+    }
     private class URLProtocolStub: URLProtocol {
         private static var stub: Stub?
         private static var requestObserver: ((URLRequest) -> Void)?
