5fe213f73aaca7fef0c78d10956c440b66b6b09b
Alex Shekunsky <oleksandr.shchekunskyi@windmill.ch>
2023-06-27 17:24:03 +0300

    Hide cache expiration details from tests with a new DSL method to protect tests from breaking changes

--- EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
+++ EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
@@ -19,10 +19,14 @@ func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
 }
 
 extension Date {
+    func minusFeedCacheMaxAge() -> Date {
+        return adding(days: -7)
+    }
+    
     func adding(days: Int) -> Date {
         return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
     }
-
+    
     func adding(seconds: TimeInterval) -> Date {
         return self + seconds
     }
