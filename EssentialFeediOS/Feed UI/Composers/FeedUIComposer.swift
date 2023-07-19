481fabcd195605ca7f00725221e449823bbcbe7c
Alex Shekunsky <oleksandr.shchekunskyi@windmill.ch>
2023-07-19 17:14:00 +0300

    Move memory management to Composer layer. The FeedPresenter shouldn't have to know or handle their dependencies lifetime.

--- EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -15,7 +15,7 @@ public final class FeedUIComposer {
         let presenter = FeedPresenter(feedLoader: feedLoader)
         let refreshController = FeedRefreshViewController(presenter: presenter)
         let feedController = FeedViewController(refreshController: refreshController)
-        presenter.loadingView = refreshController
+        presenter.loadingView = WeakRefVirtualProxy(refreshController)
         presenter.feedView = FeedViewAdapter(controller: feedController, imageLoader: imageLoader)
         return feedController
     }
@@ -24,9 +24,23 @@ public final class FeedUIComposer {
         return { [weak controller] feed in
             controller?.tableModel = feed.map { model in
                 FeedImageCellController(viewModel:
-                    FeedImageViewModel(model: model, imageLoader: loader, imageTransformer: UIImage.init))
+                                            FeedImageViewModel(model: model, imageLoader: loader, imageTransformer: UIImage.init))
             }
         }
+    }
+}
+
+private final class WeakRefVirtualProxy<T: AnyObject> {
+    private weak var object: T?
+    
+    init(_ object: T) {
+        self.object = object
+    }
+}
+
+extension WeakRefVirtualProxy: FeedLoadingView where T: FeedLoadingView {
+    func display(isLoading: Bool) {
+        object?.display(isLoading: isLoading)
     }
 }
 
@@ -38,11 +52,11 @@ private final class FeedViewAdapter: FeedView {
         self.controller = controller
         self.imageLoader = imageLoader
     }
-
+    
     func display(feed: [FeedImage]) {
         controller?.tableModel = feed.map { model in
             FeedImageCellController(viewModel:
-                FeedImageViewModel(model: model, imageLoader: imageLoader, imageTransformer: UIImage.init))
+                                        FeedImageViewModel(model: model, imageLoader: imageLoader, imageTransformer: UIImage.init))
         }
     }
 }
