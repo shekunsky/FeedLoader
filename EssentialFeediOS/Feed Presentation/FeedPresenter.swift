9befe003c62ab34e2944d9815f6d4a36ebae9e86
Alex Shekunsky <oleksandr.shchekunskyi@windmill.ch>
2023-07-28 14:52:54 +0300

    Render error message on load feed error

--- EssentialFeediOS/Feed Presentation/FeedPresenter.swift
+++ EssentialFeediOS/Feed Presentation/FeedPresenter.swift
@@ -15,9 +15,25 @@ protocol FeedView {
     func display(_ viewModel: FeedViewModel)
 }
 
+struct FeedErrorViewModel {
+    let message: String
+}
+
+protocol FeedErrorView {
+    func display(_ viewModel: FeedErrorViewModel)
+}
+
 final class FeedPresenter {
     private let feedView: FeedView
     private let loadingView: FeedLoadingView
+    
+    private let errorView: FeedErrorView
+    
+    init(feedView: FeedView, loadingView: FeedLoadingView, errorView: FeedErrorView) {
+        self.feedView = feedView
+        self.loadingView = loadingView
+        self.errorView = errorView
+    }
     
     static var title: String {
         NSLocalizedString("FEED_VIEW_TITLE",
@@ -26,9 +42,11 @@ final class FeedPresenter {
                           comment: "Title for the feed view")
     }
     
-    init(feedView: FeedView, loadingView: FeedLoadingView) {
-        self.feedView = feedView
-        self.loadingView = loadingView
+    private var feedLoadError: String {
+        return NSLocalizedString("FEED_VIEW_CONNECTION_ERROR",
+                                 tableName: "Feed",
+                                 bundle: Bundle(for: FeedPresenter.self),
+                                 comment: "Error message displayed when we can't load the image feed from the server")
     }
     
     func didStartLoadingFeed() {
@@ -41,6 +59,7 @@ final class FeedPresenter {
     }
     
     func didFinishLoadingFeed(with error: Error) {
+        errorView.display(FeedErrorViewModel(message: feedLoadError))
         loadingView.display(FeedLoadingViewModel(isLoading: false))
     }
 }
--- EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -16,7 +16,7 @@ public final class ErrorView: UIView {
 }
 
 
-public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching, FeedLoadingView {
+public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching, FeedLoadingView, FeedErrorView {
     var delegate: FeedViewControllerDelegate?
     
     public let errorView = ErrorView()
@@ -37,6 +37,10 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
         } else {
             refreshControl?.endRefreshing()
         }
+    }
+    
+    func display(_ viewModel: FeedErrorViewModel) {
+        errorView.message = viewModel.message
     }
     
     @IBAction private func refresh() {
