f953e1bb733f714ba9f80d289ae414e12dbe4822
Alex Shekunsky <oleksandr.shchekunskyi@windmill.ch>
2023-07-31 17:39:34 +0300

    Extract duplicate HTTPClientSpy helper into a shared scope

--- EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
+++ EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
@@ -9,13 +9,13 @@ import XCTest
 import EssentialFeed
 
 final class LoadFeedFromRemoteUseCaseTests: XCTestCase {
-
+    
     func test_init_doesNotRequestDataFromURL() {
         let (_, client) = makeSUT()
         
         XCTAssertTrue(client.requestedURLs.isEmpty)
     }
-
+    
     func test_load_requestsDataFromURL() {
         let url = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
@@ -86,7 +86,7 @@ final class LoadFeedFromRemoteUseCaseTests: XCTestCase {
                              description: "a description",
                              location: "a location",
                              imageURL: URL(string: "http://another-url.com")!)
-
+        
         expect(sut, toCompleteWith: .success([item1.model, item2.model]), when: {
             let json = makeItemsJson([item1.json, item2.json])
             client.complete(withStatusCode: 200, data: json)
@@ -142,7 +142,7 @@ final class LoadFeedFromRemoteUseCaseTests: XCTestCase {
     private func expect(_ sut: RemoteFeedLoader, toCompleteWith expectedResult: RemoteFeedLoader.Result, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
         
         let exp = expectation(description: "Wait for load completion")
-
+        
         sut.load { receivedResult in
             switch (expectedResult, receivedResult) {
             case let (.success(expectedItems), (.success(receivedItems))):
@@ -156,36 +156,8 @@ final class LoadFeedFromRemoteUseCaseTests: XCTestCase {
         }
         
         action()
-
+        
         wait(for: [exp], timeout: 1)
     }
     
-    private class HTTPClientSpy: HTTPClient {
-        private struct Task: HTTPClientTask {
-            func cancel() {}
-        }
-        
-        private var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
-        var requestedURLs: [URL] {
-            messages.map { $0.url }
-        }
-        
-        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
-            messages.append((url, completion))
-            return Task()
-        }
-        
-        func complete(with error: Error, at index: Int = 0) {
-            messages[index].completion(.failure(error))
-        }
-        
-        func complete(withStatusCode code: Int, data: Data, at index: Int = 0) {
-            let response = HTTPURLResponse(url: requestedURLs[index],
-                                           statusCode: code,
-                                           httpVersion: nil,
-                                           headerFields: nil)!
-            
-            messages[index].completion(.success((data, response)))
-        }
-    }
 }
