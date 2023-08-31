f085aadc7911eaed017f4be8404f84946dacaa1c
Alex Shekunsky <oleksandr.shchekunskyi@windmill.ch>
2023-08-31 15:57:48 +0300

    Localize image comments title

---
+++ EssentialFeed/EssentialFeedTests/Image Comments Presentation/ImageCommentsLocalizationTests.swift
@@ -0,0 +1,20 @@
+//
+//  ImageCommentsLocalizationTests.swift
+//  EssentialFeedTests
+//
+//  Created by Alex2 on 31.08.2023.
+//
+
+import XCTest
+import EssentialFeed
+
+class ImageCommentsLocalizationTests: XCTestCase {
+    
+    func test_localizedStrings_haveKeysAndValuesForAllSupportedLocalizations() {
+        let table = "ImageComments"
+        let bundle = Bundle(for: ImageCommentsPresenter.self)
+        
+        assertLocalizedKeyAndValuesExist(in: bundle, table)
+    }
+    
+}
