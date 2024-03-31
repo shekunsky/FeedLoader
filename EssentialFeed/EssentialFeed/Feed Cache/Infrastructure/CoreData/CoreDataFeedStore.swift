2ddc54e2e8671dba55bc1f6b1e59abc93e22325f
Alex Shekunsky <oleksandr.shchekunskyi@windmill.ch>
2024-03-31 17:30:24 +0300

    Remove performSync method

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -14,7 +14,7 @@ public final class CoreDataFeedStore {
     private static let model = NSManagedObjectModel.with(name: modelName, in: Bundle(for: CoreDataFeedStore.self))
     
     private let container: NSPersistentContainer
-    private let context: NSManagedObjectContext
+    let context: NSManagedObjectContext
     
     public enum ContextQueue {
         case main
@@ -36,18 +36,6 @@ public final class CoreDataFeedStore {
         } catch {
             throw StoreError.failedToLoadPersistentContainer(error)
         }
-    }
-    
-    func performAsync(_ action: @escaping (NSManagedObjectContext) -> Void) {
-        let context = self.context
-        context.perform { action(context) }
-    }
-    
-    func performSync<R>(_ action: (NSManagedObjectContext) -> Result<R, Error>) throws -> R {
-        let context = self.context
-        var result: Result<R, Error>!
-        context.performAndWait { result = action(context) }
-        return try result.get()
     }
     
     public func perform(_ action: @escaping () -> Void) {
